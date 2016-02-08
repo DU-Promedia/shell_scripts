7fd3b400e0f8285cb27b03d03da50a6b4216dc32
Kent Cederström <kent@kentcederstrom.se>
2016-02-08 13:51:19 +0100

    new clean up

--- cleanup_docker_images.sh
+++ cleanup_docker_images.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "Cleaning up docker images"
-
+echo "Cleaning Docker Images"
 docker rmi -f $(docker images | grep "^<none>" | awk '{print $3}')
+
+echo "REALLY Cleaning Docker Images"
+docker rm -v $(docker ps -a -q -f status=exited)
